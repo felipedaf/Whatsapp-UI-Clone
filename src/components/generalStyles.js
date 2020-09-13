18cd88671f045fa5989cceffe79766e530032afd
FelipeAmorimFerreira <felipe.amorim.ferreira@ccc.ufcg.edu.br>
2020-09-13 15:56:27 -0300

    Adding and modifying overall styles

---
+++ src/components/generalStyles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+
+export const FSection = styled.div`
+  min-width: 260px;
+  min-height: 100vh;
+  width: calc(260px + 32%);
+`
+
+export const SSection = styled.div`
+  min-width: 390px;
+  min-height: 100vh;
+  width: calc(390px + 68%);
+`
