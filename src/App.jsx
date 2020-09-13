40a884e6402f9cacdae2c60b375d30907188bf14
FelipeAmorimFerreira <felipe.amorim.ferreira@ccc.ufcg.edu.br>
2020-09-13 01:07:21 -0300

    Colors theme logic added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import './App.css'
+import React, { useState } from "react";
+import ThemeContext, { Theme } from "./utils/theme";
+import ToggleButton from "./components/ToggleButton";
+import Background from "./components/Background"
 
-function App() {
+const App = () => {
+  const [theme, setTheme] = useState(Theme.getTheme());
+
+  const changeTheme = newTheme => {
+    setTheme(() => {
+      Theme.updateTheme(newTheme);
+      return newTheme;
+    });
+  };
+
   return (
-    <div>
-      Opa teste
-    </div>
+    <>
+      <ThemeContext.Provider value={theme}>
+        <Background>
+          <ToggleButton toggle={changeTheme}/>
+          <p>O tema é: {theme}</p>
+        </Background>
+      </ThemeContext.Provider>
+    
+    </>
   );
 }
 
--- src/utils/theme/index.js
+++ src/utils/theme/index.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 const THEME_KEY = "color_theme";
 
 export const DARK_MODE_COLORS = {
@@ -17,18 +19,25 @@ export const Theme = {
 
   getTheme: () => {
     try {
-      return window.localStorage.getItem(THEME_KEY);
+      const theme = window.localStorage.getItem(THEME_KEY);
+
+      if (!theme)
+        throw new Error("There's no theme");
+        
+      return theme
     } catch(e) {
-      return null
+      Theme.updateTheme(THEMES.LIGHT_THEME);
+      return window.localStorage.getItem(THEME_KEY);
     }
   },
-  updateTheme: value => {
+  updateTheme: newTheme => {
     try {
-      window.localStorage.setItem(THEME_KEY, value)
-      return true
+      window.localStorage.setItem(THEME_KEY, newTheme);
+      return true;
     } catch(e) {
-      return false
+      return false;
     }
   },
 };
 
+export default React.createContext();
